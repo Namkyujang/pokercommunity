5de7a18ecfd9435de2bb129d991bb45b96d7d5b9
Namkyujang <98171217+Namkyujang@users.noreply.github.com>
2022-01-22 21:20:46 +0900

    making gmae

--- play.py
+++ play.py
@@ -1,5 +1,5 @@
 import random
-def game1():
+def game1(credit):
     s=['Sa','S2','S3','S4','S5','S6','S7','S8','S9','S10','SK','SQ','SJ']
     d=['Da','D2','D3','D4','D5','D6','D7','D8','D9','D10','DK','DQ','DJ']
     h=['Ha','H2','H3','H4','H5','H6','H7','H8','H9','H10','HK','HQ','HJ']
@@ -9,4 +9,17 @@ def game1():
     random.shuffle(pack)
     opphand=pack[2]
     myhand=pack[2:4]
-    
+    coke==True
+    while coke==True:
+        mybet=int(input('How much you gonna bet:'))
+        if mybet<credit:
+            credit=credit-mybet
+            hisbet=mybet*2
+            pool=mybet+hisbet
+            coke==False
+        else:
+            print('Too much')
+    print(pool)
+    print(pack[4:7])
+    while pick==True:
+        
