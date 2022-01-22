f9c9a169c4f719942d93015ff16c2b01f5c44b30
Namkyujang <98171217+Namkyujang@users.noreply.github.com>
2022-01-22 17:49:14 +0900

    Changes

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ import start, opp
 credit=1000
 name=start.start()
 status=True
-while (status or credit<1000):
-    opp.opp(credit)
+while (status or credit>1000):
+    play=opp.opp(credit)
+    if play==True:
+        print('Lets play')
+
+if status==False:
+    print('YOu won')
+elif credit<1000:
+    print("you lost")
